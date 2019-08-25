807f7ad6668c455e36955afd0009c5252a5179d7
op_1127 <oplasencia27@gmail.com>
2019-08-24 20:01:19 -0700

    changed wavs to mp3

--- src/js/music.js
+++ src/js/music.js
@@ -4,135 +4,135 @@ export var drumKit = {
         pad: 'z',
         color: '#1a1c2c',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Kick 1.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Kick.mp3'],
+            volume: 0.7
         })
     },
     x: {
         pad: 'x',
         color: '#572956',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Snare 6.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Snare.mp3'],
+            volume: 0.7
         })
     },
     c: {
         pad: 'c',
         color: '#b14156',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Top 8.wav'],
-            volume: 0.2
+            src: ['sounds/kit/Hihat.mp3'],
+            volume: 0.7
         })
     },
     v: {
         pad: 'v',
         color: '#ee7b58',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Top 11.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Hihat2.mp3'],
+            volume: 0.7
         })
     },
     a: {
         pad: 'a',
         color: '#ffd079',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Kick 12.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Kick2.mp3'],
+            volume: 0.7
         })
     },
     s: {
         pad: 's',
         color: '#a0f072',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Snare 14.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Clap.mp3'],
+            volume: 0.7
         })
     },
     d: {
         pad: 'd',
         color: '#38b86e',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Top 9.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Rim.mp3'],
+            volume: 0.7
         })
     },
     f: {
         pad: 'f',
         color: '#276e7b',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Top 13.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Snare2.mp3'],
+            volume: 0.7
         })
     },
     q: {
         pad: 'q',
         color: '#726da8',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Perc 13.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Kick3.mp3'],
+            volume: 0.7
         })
     },
     w: {
         pad: 'w',
         color: '#7d8cc4',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Perc 12.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Shaker.mp3'],
+            volume: 0.7
         })
     },
     e: {
         pad: 'e',
         color: '#a0d2db',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Perc 6.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Clave.mp3'],
+            volume: 0.7
         })
     },
     r: {
         pad: 'r',
         color: '#bee7e8',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_Perc 15.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Cowbell.mp3'],
+            volume: 0.7
         })
     },
     1: {
         pad: '1',
         color: '#f08700',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_125_Amin_Crazy Vocal 1.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Conga1.mp3'],
+            volume: 0.7
         })
     },
     2: {
         pad: '2',
         color: '#f49f0a',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_125_Amin_Crazy Vocal 3.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Conga2.mp3'],
+            volume: 0.7
         })
     },
     3: {
         pad: '3',
         color: '#efca08',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_125_Amin_Crazy Vocal 5.wav'],
-            volume: 0.3
+            src: ['sounds/kit/Conga3.mp3'],
+            volume: 0.7
         })
     },
     4: {
         pad: '4',
         color: '#00a6a6',
         sound: new Howl({
-            src: ['sounds/rnb_kit/OS_RNB_125_Amin_Crazy Vocal 6.wav'],
-            volume: 1.0
+            src: ['sounds/kit/Cymbal.mp3'],
+            volume: 0.7
         })
     }
 }
 
 export var song = {
     sound: new Howl({
-        src: ['sounds/songs/future-rnb.wav'],
+        src: ['sounds/songs/future-rnb.mp3'],
         volume: 1.0,
         onend: function () {
             playback = false;
