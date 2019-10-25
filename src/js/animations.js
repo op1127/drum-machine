87e165074782447e55e1607d7cb9988d9925d42d
op_1127 <oplasencia27@gmail.com>
2019-10-24 23:30:22 -0700

    random color add

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,12 +1,10 @@
-import paper from 'paper';
-import {
-    drumKit
-} from './music'
+import paper from "paper";
+import { drumKit } from "./music";
 
 paper.install(window);
 
 // Get a reference to the canvas object
-var canvas = document.getElementById('myCanvas');
+var canvas = document.getElementById("myCanvas");
 // Create an empty project and a view for the canvas:
 paper.setup(canvas);
 
@@ -14,60 +12,69 @@ paper.setup(canvas);
 var shapes = [];
 var rotateNum;
 
+function randomColor() {
+	var colors = ["#0fa3b1", "#d00000", "#f6511d", "#004385", "#06d6a0"];
+	var randomNum = Math.floor(Math.random() * 5);
+
+	return colors[randomNum];
+}
+
 // Called by the triggerPad function below
 function changeColor(key, color) {
-    var padClass = ".pad-" + key;
-    var el = document.querySelector(padClass);
-    el.style.backgroundColor = color;
-    el.style.color = color;
-    el.style.transform = 'scale(.94)';
+	var padClass = ".pad-" + key;
+
+	var el = document.querySelector(padClass);
+	el.style.backgroundColor = color;
+	el.style.color = color;
+	el.style.transform = "scale(.94)";
 }
 
 export function resetColor(key) {
-    var padClass = ".pad-" + key;
-    var el = document.querySelector(padClass);
-    el.style.backgroundColor = 'rgb(238, 238, 238)';
-    el.style.color = 'rgb(110, 110, 110)';
-    el.style.transform = 'scale(1)';
+	var padClass = ".pad-" + key;
+	var el = document.querySelector(padClass);
+	el.style.backgroundColor = "rgb(238, 238, 238)";
+	el.style.color = "rgb(110, 110, 110)";
+	el.style.transform = "scale(1)";
 }
 
-
 export function triggerPad(event) {
-    var maxPoint = new Point(view.size.width, view.size.height);
-    var randomPoint = Point.random();
-    var point = {
-        x: maxPoint.x * randomPoint.x,
-        y: maxPoint.y * randomPoint.y
-    };
-    var num = Math.floor(Math.random() * 3);
-    var newShape;
+	var maxPoint = new Point(view.size.width, view.size.height);
+	var randomPoint = Point.random();
+	var point = {
+		x: maxPoint.x * randomPoint.x,
+		y: maxPoint.y * randomPoint.y
+	};
+	var num = Math.floor(Math.random() * 3);
+	var newShape;
 
-    // Determines a the shape using the random num
-    if (num === 0) {
-        newShape = new Path.Circle(point, 550);
-    } else if (num === 1) {
-        newShape = new Path.RegularPolygon(point, 3, 800);
-        rotateNum = -5;
-    } else if (num === 2) {
-        newShape = new Path.Rectangle(point, 800, 800);
-        rotateNum = 5;
+	// Determines a the shape using the random num
+	if (num === 0) {
+		newShape = new Path.Circle(point, 550);
+	} else if (num === 1) {
+		newShape = new Path.RegularPolygon(point, 3, 800);
+		rotateNum = -5;
+	} else if (num === 2) {
+		newShape = new Path.Rectangle(point, 800, 800);
+		rotateNum = 5;
     }
-    newShape.strokeWidth = 25;
-    newShape.strokeColor = drumKit[event].color
-    drumKit[event].sound.play();
-    shapes.push(newShape);
-    changeColor(drumKit[event].pad, drumKit[event].color);
+    var newColor = randomColor();
+
+	newShape.strokeWidth = 25;
+	newShape.strokeColor = newColor;
+	drumKit[event].sound.play();
+	shapes.push(newShape);
+	changeColor(drumKit[event].pad, newColor);
 }
 
 // Paper js animations
-view.onFrame = function () {
-    for (var i = 0; i < shapes.length; i++) {
-        shapes[i].scale(0.9);
-        shapes[i].rotate(rotateNum);
-        
-        if (shapes[i].area < 1) {
-            shapes[i].remove(); // remove the shape from the canvas
-            shapes.splice(i, 1); // remove the shape from the array
-        }
-    }
-}
+view.onFrame = function() {
+	for (var i = 0; i < shapes.length; i++) {
+		shapes[i].scale(0.9);
+		shapes[i].rotate(rotateNum);
+
+		if (shapes[i].area < 1) {
+			shapes[i].remove(); // remove the shape from the canvas
+			shapes.splice(i, 1); // remove the shape from the array
+		}
+	}
+};
--- src/js/music.js
+++ src/js/music.js
@@ -1,148 +1,129 @@
-import {
-    setPlayback,
-    loop
-} from './index';
+import { setPlayback, loop } from "./index";
 
 // Object which contains pad value, animation color, and sound
 export var drumKit = {
-    z: {
-        pad: 'z',
-        color: '#007EA7',
-        sound: new Howl({
-            src: ['sounds/kit/Kick.mp3'],
-            volume: 1.0
-        })
-    },
-    x: {
-        pad: 'x',
-        color: '#007398 ',
-        sound: new Howl({
-            src: ['sounds/kit/Snare.mp3'],
-            volume: 1.0
-        })
-    },
-    c: {
-        pad: 'c',
-        color: '#006889',
-        sound: new Howl({
-            src: ['sounds/kit/Hihat.mp3'],
-            volume: 1.0
-        })
-    },
-    v: {
-        pad: 'v',
-        color: '#005C7A',
-        sound: new Howl({
-            src: ['sounds/kit/Hihat2.mp3'],
-            volume: 1.0
-        })
-    },
-    a: {
-        pad: 'a',
-        color: '#B80204',
-        sound: new Howl({
-            src: ['sounds/kit/Kick2.mp3'],
-            volume: 1.0
-        })
-    },
-    s: {
-        pad: 's',
-        color: '#BE191A',
-        sound: new Howl({
-            src: ['sounds/kit/Clap.mp3'],
-            volume: 1.0
-        })
-    },
-    d: {
-        pad: 'd',
-        color: '#B80204',
-        sound: new Howl({
-            src: ['sounds/kit/Rim.mp3'],
-            volume: 1.0
-        })
-    },
-    f: {
-        pad: 'f',
-        color: '#A80204',
-        sound: new Howl({
-            src: ['sounds/kit/Snare2.mp3'],
-            volume: 1.0
-        })
-    },
-    q: {
-        pad: 'q',
-        color: '#6369D1',
-        sound: new Howl({
-            src: ['sounds/kit/Kick3.mp3'],
-            volume: 1.0
-        })
-    },
-    w: {
-        pad: 'w',
-        color: '#5A60BE',
-        sound: new Howl({
-            src: ['sounds/kit/Shaker.mp3'],
-            volume: 1.0
-        })
-    },
-    e: {
-        pad: 'e',
-        color: '#5256AC',
-        sound: new Howl({
-            src: ['sounds/kit/Clave.mp3'],
-            volume: 1.0
-        })
-    },
-    r: {
-        pad: 'r',
-        color: '#494D99',
-        sound: new Howl({
-            src: ['sounds/kit/Cowbell.mp3'],
-            volume: 1.0
-        })
-    },
-    1: {
-        pad: '1',
-        color: '#4BB3FD',
-        sound: new Howl({
-            src: ['sounds/kit/Conga1.mp3'],
-            volume: 1.0
-        })
-    },
-    2: {
-        pad: '2',
-        color: '#45A3E6',
-        sound: new Howl({
-            src: ['sounds/kit/Conga2.mp3'],
-            volume: 1.0
-        })
-    },
-    3: {
-        pad: '3',
-        color: '#3E93D0',
-        sound: new Howl({
-            src: ['sounds/kit/Conga3.mp3'],
-            volume: 1.0
-        })
-    },
-    4: {
-        pad: '4',
-        color: '#3783B9',
-        sound: new Howl({
-            src: ['sounds/kit/Cymbal.mp3'],
-            volume: 1.0
-        })
-    }
-}
+	z: {
+		pad: "z",
+		sound: new Howl({
+			src: ["sounds/kit/Kick.mp3"],
+			volume: 1.0
+		})
+	},
+	x: {
+		pad: "x",
+		sound: new Howl({
+			src: ["sounds/kit/Snare.mp3"],
+			volume: 1.0
+		})
+	},
+	c: {
+		pad: "c",
+		sound: new Howl({
+			src: ["sounds/kit/Hihat.mp3"],
+			volume: 1.0
+		})
+	},
+	v: {
+		pad: "v",
+		sound: new Howl({
+			src: ["sounds/kit/Hihat2.mp3"],
+			volume: 1.0
+		})
+	},
+	a: {
+		pad: "a",
+		sound: new Howl({
+			src: ["sounds/kit/Kick2.mp3"],
+			volume: 1.0
+		})
+	},
+	s: {
+		pad: "s",
+		sound: new Howl({
+			src: ["sounds/kit/Clap.mp3"],
+			volume: 1.0
+		})
+	},
+	d: {
+		pad: "d",
+		sound: new Howl({
+			src: ["sounds/kit/Rim.mp3"],
+			volume: 1.0
+		})
+	},
+	f: {
+		pad: "f",
+		sound: new Howl({
+			src: ["sounds/kit/Snare2.mp3"],
+			volume: 1.0
+		})
+	},
+	q: {
+		pad: "q",
+		sound: new Howl({
+			src: ["sounds/kit/Kick3.mp3"],
+			volume: 1.0
+		})
+	},
+	w: {
+		pad: "w",
+		sound: new Howl({
+			src: ["sounds/kit/Shaker.mp3"],
+			volume: 1.0
+		})
+	},
+	e: {
+		pad: "e",
+		sound: new Howl({
+			src: ["sounds/kit/Clave.mp3"],
+			volume: 1.0
+		})
+	},
+	r: {
+		pad: "r",
+		sound: new Howl({
+			src: ["sounds/kit/Cowbell.mp3"],
+			volume: 1.0
+		})
+	},
+	1: {
+		pad: "1",
+		sound: new Howl({
+			src: ["sounds/kit/Conga1.mp3"],
+			volume: 1.0
+		})
+	},
+	2: {
+		pad: "2",
+		sound: new Howl({
+			src: ["sounds/kit/Conga2.mp3"],
+			volume: 1.0
+		})
+	},
+	3: {
+		pad: "3",
+		sound: new Howl({
+			src: ["sounds/kit/Conga3.mp3"],
+			volume: 1.0
+		})
+	},
+	4: {
+		pad: "4",
+		sound: new Howl({
+			src: ["sounds/kit/Cymbal.mp3"],
+			volume: 1.0
+		})
+	}
+};
 
 export var song = {
-    sound: new Howl({
-        src: ['sounds/songs/hype.mp3'],
-        volume: 1.0,
-        onend: function () {
-            if (!loop) {
-                setPlayback(false);
-            }
-        }
-    }),
-}
+	sound: new Howl({
+		src: ["sounds/songs/hype.mp3"],
+		volume: 1.0,
+		onend: function() {
+			if (!loop) {
+				setPlayback(false);
+			}
+		}
+	})
+};
